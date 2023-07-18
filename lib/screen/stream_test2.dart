bf6f44969e517cd00a39b7cb30b72260c5f8a662
Bansook Nam <bs.nam@lawfully.com>
2023-07-18 23:57:35 +0900

    generator

--- lib/screen/stream_test2.dart
+++ lib/screen/stream_test2.dart
@@ -1,18 +1,36 @@
-import 'dart:io';
-
 import 'package:fast_app_base/common/dart/extension/num_duration_extension.dart';
-
-import 'future_test.dart';
 
 void main() async {
   /// List와 Iterable
+  // final List list = ['blue', 'yellow', 'red'];
+  //
+  // list.forEach((element) {
+  //   print(element);
+  // });
+  // for (final color in [1, 2, 3, 4, 5]) {
+  //   print(color);
+  // }
 
   /// sync*로 Iterable 만들기
+
+  await for (final message in countStream(3)) {
+    print(message);
+  }
 
   /// async*로 Stream 만들기
 
   /// yield*를 통해서 Iterable & Stream 연장시키기
 }
+
+Iterable<String> countIterable(int max) sync* {
+  for (int i = 1; i <= max; i++) {
+    yield i.toString();
+  }
+  yield '새해복 많이 받으세요';
+  yield* ['a', 'b', 'c'];
+}
+
+///Generator
 
 Stream<String> countStream(int to) async* {
   for (int i = 1; i <= to; i++) {
@@ -20,13 +38,9 @@ Stream<String> countStream(int to) async* {
     yield i.toString();
   }
   yield '새해복 많이 받으세요';
+  yield* countStream(to);
 }
 
-Iterable<String> countIterable(int max) sync* {
-  for (int i = 1; i <= max; i++) {
-    sleep(1.seconds);
-    yield i.toString();
-    yield* ["4", "5", "6", "5"];
-  }
-  yield '새해복 많이 받으세요';
+Future sleepAsync(Duration duration) {
+  return Future.delayed(duration, () => {});
 }
