6455e18fb21a9b13161e746466ec8ac98ac7afb9
Bansook Nam <bs.nam@lawfully.com>
2023-07-30 16:42:14 +0900

    역할 분리=

--- lib/class/ch04_advanced_dart1/object_oriented_programming/solid/1_srp/1_srp.dart
+++ lib/class/ch04_advanced_dart1/object_oriented_programming/solid/1_srp/1_srp.dart
@@ -1,1 +1,12 @@
-main() {}
+import 'package:fast_app_base/class/ch04_advanced_dart1/object_oriented_programming/solid/1_srp/local_repository.dart';
+import 'package:fast_app_base/class/ch04_advanced_dart1/object_oriented_programming/solid/1_srp/todo_note.dart';
+
+main() {
+  final todo = TodoNote();
+  final repository = LocalRepository();
+  todo.setContent('수학 과제', "p20 ~ p40를 공책에 작성");
+
+  repository.save(todo);
+  repository.modify(todo);
+  repository.remove(todo);
+}
